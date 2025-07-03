4243a7c137c255591b131bf2e7b4251c7bd1e5e5
dJPoida <djpoida@gmail.com>
2025-07-03 22:33:03 +1000

    Updated game code word library.

--- src/shared/constants/word-lists.const.ts
+++ src/shared/constants/word-lists.const.ts
@@ -1,20 +1,43 @@
 export const ADJECTIVES = [
-  'Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Black', 'White', 'Gray',
-  'Quick', 'Slow', 'Big', 'Small', 'Tall', 'Short', 'Fat', 'Thin', 'Round', 'Square',
-  'Happy', 'Sad', 'Angry', 'Joyful', 'Brave', 'Cowardly', 'Honest', 'Deceitful',
-  'Bright', 'Dark', 'Light', 'Heavy', 'Loud', 'Quiet', 'Fast', 'Gentle',
-  'Fierce', 'Calm', 'Wild', 'Tame', 'Clever', 'Dull', 'Sharp', 'Blunt'
+  'Wobbly', 'Jiggly', 'Squishy', 'Bouncy', 'Stretchy', 'Gooey', 'Sticky', 'Slippery', 'Mushy', 'Lumpy',
+  'Chunky', 'Crusty', 'Crumbly', 'Flaky', 'Soggy', 'Drippy', 'Oozy', 'Bubbly', 'Fizzy', 'Foamy',
+  'Giggly', 'Sneezy', 'Sniffly', 'Hiccupy', 'Burpy', 'Yawny', 'Sleepy', 'Dreamy', 'Dopey', 'Dizzy',
+  'Loopy', 'Nutty', 'Bonkers', 'Wacky', 'Zany', 'Goofy', 'Silly', 'Ridiculous', 'Absurd', 'Preposterous',
+  'Ticklish', 'Itchy', 'Scratchy', 'Prickly', 'Bumpy', 'Lumpy', 'Wrinkly', 'Crinkly', 'Ruffled', 'Disheveled',
+  'Lopsided', 'Crooked', 'Wonky', 'Tilted', 'Twisted', 'Bent', 'Warped', 'Squished', 'Flattened', 'Puffed',
+  'Chubby', 'Plump', 'Tubby', 'Stocky', 'Stumpy', 'Stubby', 'Pudgy', 'Rotund', 'Portly', 'Hefty',
+  'Teeny', 'Itty-bitty', 'Wee', 'Tiny', 'Microscopic', 'Minuscule', 'Teensy', 'Petite', 'Compact', 'Miniature',
+  'Gigantic', 'Enormous', 'Humongous', 'Colossal', 'Mammoth', 'Gargantuan', 'Titanic', 'Whopping', 'Massive', 'Immense',
+  'Sparkly', 'Glittery', 'Shimmery', 'Twinkly', 'Glowing', 'Radiant', 'Dazzling', 'Brilliant', 'Luminous', 'Gleaming'
 ];
 
 export const NOUNS = [
-  'Fox', 'Dog', 'Cat', 'Lion', 'Tiger', 'Bear', 'Wolf', 'Eagle', 'Hawk', 'Shark',
-  'Whale', 'Monkey', 'Gorilla', 'Panda', 'Koala', 'Kangaroo', 'Gopher', 'Badger',
-  'Snail', 'Wasp', 'Bee', 'Ant', 'Spider', 'Scorpion', 'Snake', 'Lizard', 'Frog',
-  'Fish', 'Bird', 'Dragon', 'Unicorn', 'Griffin', 'Phoenix', 'Hydra', 'Goblin'
+  'Banana', 'Pickle', 'Doughnut', 'Marshmallow', 'Pretzel', 'Waffle', 'Pancake', 'Taco', 'Burrito', 'Spaghetti',
+  'Meatball', 'Cupcake', 'Jellybean', 'Gumball', 'Lollipop', 'Bubblegum', 'Popcorn', 'Cheese', 'Bacon', 'Sardine',
+  'Noodle', 'Breadstick', 'Muffin', 'Bagel', 'Croissant', 'Biscuit', 'Cracker', 'Nacho', 'Salami', 'Pickle',
+  'Mustache', 'Eyebrow', 'Bellybutton', 'Elbow', 'Kneecap', 'Nostril', 'Earlobe', 'Dimple', 'Freckle', 'Wrinkle',
+  'Toothbrush', 'Rubber Duck', 'Toilet Paper', 'Spatula', 'Whisk', 'Spoon', 'Ladle', 'Colander', 'Plunger', 'Mop',
+  'Doorknob', 'Paperclip', 'Stapler', 'Eraser', 'Pencil', 'Crayon', 'Marker', 'Sticker', 'Glue', 'Tape',
+  'Sock', 'Underwear', 'Pajamas', 'Slippers', 'Mittens', 'Scarf', 'Hat', 'Wig', 'Toupee', 'Sunglasses',
+  'Balloon', 'Yo-yo', 'Slinky', 'Kazoo', 'Whistle', 'Harmonica', 'Tambourine', 'Accordion', 'Tuba', 'Banjo',
+  'Piggybank', 'Rubber Band', 'Bubble', 'Feather', 'Pebble', 'Marble', 'Button', 'Zipper', 'Velcro', 'Magnet',
+  'Lampshade', 'Doorstop', 'Coaster', 'Napkin', 'Towel', 'Pillow', 'Cushion', 'Blanket', 'Teddy Bear', 'Doll',
+  'Clown', 'Jester', 'Goblin', 'Troll', 'Fairy', 'Gnome', 'Elf', 'Dwarf', 'Giant', 'Wizard'
 ];
 
 export const VERBS = [
-  'Jumping', 'Running', 'Sleeping', 'Eating', 'Drinking', 'Flying', 'Swimming', 'Walking',
-  'Talking', 'Singing', 'Dancing', 'Fighting', 'Working', 'Playing', 'Thinking', 'Dreaming',
-  'Hoping', 'Wishing', 'Laughing', 'Crying', 'Screaming', 'Shouting', 'Whispering', 'Watching'
+  'Sneezing', 'Burping', 'Hiccuping', 'Giggling', 'Snorting', 'Cackling', 'Chuckling', 'Guffawing',
+  'Wiggling', 'Jiggling', 'Wobbling', 'Bouncing', 'Prancing', 'Skipping', 'Hopping', 'Galloping',
+  'Tiptoeing', 'Shuffling', 'Stumbling', 'Bumbling', 'Fumbling', 'Tumbling', 'Grumbling', 'Mumbling',
+  'Babbling', 'Rambling', 'Blabbering', 'Sputtering', 'Stuttering', 'Muttering', 'Chattering', 'Nattering',
+  'Slurping', 'Chomping', 'Munching', 'Crunching', 'Gobbling', 'Nibbling', 'Gnawing', 'Devouring',
+  'Belching', 'Yawning', 'Stretching', 'Scratching', 'Itching', 'Twitching', 'Fidgeting', 'Squirming',
+  'Squeaking', 'Squealing', 'Honking', 'Tooting', 'Beeping', 'Booping', 'Poking', 'Prodding',
+  'Tickling', 'Pinching', 'Squeezing', 'Hugging', 'Cuddling', 'Snuggling', 'Nuzzling', 'Puzzling',
+  'Drooling', 'Slobbering', 'Dribbling', 'Spitting', 'Spraying', 'Splashing', 'Splattering', 'Spluttering',
+  'Waddling', 'Swaggering', 'Strutting', 'Mincing', 'Flouncing', 'Prancing', 'Lurching', 'Careening',
+  'Sniffling', 'Snuffling', 'Whimpering', 'Whining', 'Pouting', 'Sulking', 'Moping', 'Brooding',
+  'Plopping', 'Flopping', 'Dropping', 'Stopping', 'Hopping', 'Bopping', 'Popping', 'Chopping',
+  'Flapping', 'Clapping', 'Slapping', 'Tapping', 'Zapping', 'Snapping', 'Wrapping', 'Mapping',
+  'Buzzing', 'Fizzing', 'Sizzling', 'Drizzling', 'Fizzling', 'Puzzling', 'Dazzling', 'Razzling'
 ]; 
